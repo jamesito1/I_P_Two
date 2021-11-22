f87bc41ad52c5f830ca6df324a24e976d2608d22
jamesito1 <jamespesca2004@gmail.com>
2021-11-21 20:13:58 -0500

    asco git

---
+++ Assets/Js/main.js
@@ -0,0 +1,13 @@
+let video= document.getElementById("video_inicial")
+let body = document.querySelector(".body")
+
+setTimeout(video1,27000);
+
+
+
+function video1 (){
+
+video.className+=(" video_desaparece");
+body.className+= (" body_a")
+console.log(video);
+}
